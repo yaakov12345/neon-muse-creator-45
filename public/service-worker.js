2a2af6def9d8ff7fd759fdb0b039b6186eeb3bef
yaakov12345 <123314832+yaakov12345@users.noreply.github.com>
2026-05-11 10:24:33 +0300

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "flame-app-cache-v1";
+
+const urlsToCache = [
+  "/",
+  "/manifest.json",
+  "/favicon.ico"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
